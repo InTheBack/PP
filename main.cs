44f3314a10e3ed73d5a97cd28288411a84222cff
Nikita <rudenkonikita098@mail.ru>
2023-02-16 14:02:06 +0300

    Added comma.

--- main.cs
+++ main.cs
@@ -1,3 +1,5 @@
+using system;
+
 namespace Git
 {
    class Git
@@ -5,6 +7,10 @@ namespace Git
     static void Main(string[] args)
       {
       System.Console.WriteLine("Hello Git");
+      System.Console.WriteLine("Enter your name: Nikita");
+	  string name = Console.ReadLine();
+	  System.Console.WriteLine(name.Length);
+	  System.Console.WriteLine($"Hello (Nikita)!");
       }
    }
 }
